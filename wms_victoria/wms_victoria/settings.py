f2ceb44a5ea22ea3f3c5a7fadd186759fe21c56e
Kevin <kevinogutu263@gmail.com>
2024-08-19 14:26:06 +0300

    Deploying WMS to Heroku

--- wms_victoria/wms_victoria/settings.py
+++ wms_victoria/wms_victoria/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -144,3 +144,5 @@ DEFAULT_FROM_EMAIL = 'kevinogutu263@gmail.com'  # Default sender email address
 ADMIN_EMAIL = 'kevinogutu263@gmail.com'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+django_heroku.settings(locals())
